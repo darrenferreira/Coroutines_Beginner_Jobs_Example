cb65311cc8b9d876f30582fb7a7017c011b276ec
darren.ferreira <darren.ferreira@standardbank.co.za>
2020-01-22 09:17:48 +0200

    adds a job that updates the UI progress bar

--- app/src/main/java/phone/com/coroutinesjobs/MainActivity.kt
+++ app/src/main/java/phone/com/coroutinesjobs/MainActivity.kt
@@ -1,8 +1,13 @@
 package phone.com.coroutinesjobs
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.CompletableJob
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +20,68 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        job_button.setOnClickListener {
+            if (!::job.isInitialized) {
+                initJob()
+            }
+        }
+        job_progress_bar.startJobOrCancel(job)
 
+    }
 
+    fun ProgressBar.startJobOrCancel(job: Job) {
+        if (this.progress > 0) {
+            println("${job} is already active...Cancelling....")
+            resetJob()
+        } else {
+            job_button.setText("Cancel job #1")
+            CoroutineScope(IO + job).launch {
+                println("coroutine ${this} is activated with this $job")
+
+                for (i in PROG_START..PROG_MAX){
+                    delay((JOB_TIME / PROG_MAX).toLong())
+                    this@startJobOrCancel.progress = i
+                }
+
+                updateJobCompleteTextView("Job is complete")
+            }
+        }
+    }
+
+    private fun updateJobCompleteTextView(text :String){
+        GlobalScope.launch(Main) {
+            job_complete_text.setText(text)
+        }
+    }
+
+    private fun resetJob() {
+        if (job.isActive||job.isCompleted){
+            job.cancel(CancellationException("Resetting Job"))
+        }
+        initJob()
+    }
+
+    fun initJob() {
+        job_button.text = "Start Job 1"
+        updateJobCompleteTextView("")
+        job = Job()
+        job.invokeOnCompletion {
+            it?.message.let {
+                var msg = it
+                if (msg.isNullOrBlank()) {
+                    msg = "unknown cancellation error"
+                }
+                println("${job} was cancelled. Reason : ${msg}")
+                showToast(msg)
+            }
+        }
+        job_progress_bar.max = PROG_MAX
+        job_progress_bar.progress = PROG_START
+    }
+
+    fun showToast(text: String) {
+        GlobalScope.launch(Main) {
+            Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
